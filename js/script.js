ccfa6f5602ead2e6d432fa9147020de6247f73e2
Nick Muturi <nick@localhost.localdomain>
2018-07-14 22:34:42 +0300

    css: add error animation, script:substitute previous error handler with a better alternative (fixing a bug)

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
 
   $("form#pingPongForm").submit(function(event){
     event.preventDefault();
+
     var userNumber = parseInt($("input#userInput").val());
 
     $("ul#gameOutput").empty(); //Clears previous List
-    errorHandler(userNumber); //Handles invalid input
+    $("#userInput").removeClass("error"); //Removes error class if previous input had an error
+    if(!Number.isInteger(userNumber)||userNumber<0||userNumber===""){    //Error Handler
+      $("#userInput").addClass("error");
+      return false;
+    }
 
     //Loop through the numbers & calls the functions
     for (var loopVar=1;loopVar<=userNumber;loopVar++){
@@ -13,10 +18,10 @@ $(document).ready(function(){
         $("ul#gameOutput").append("<li>"+"Ping Pong!"+"</li>");
       }
       else if(checkThree(loopVar)){
-        $("ul#gameOutput").append("<li>Ping!</li>");
+        $("ul#gameOutput").append("<li>Ping</li>");
       }
       else if(checkFive(loopVar)){
-        $("ul#gameOutput").append("<li>Pong!</li>");
+        $("ul#gameOutput").append("<li>Pong</li>");
       }
       else {
         $("ul#gameOutput").append("<li>"+loopVar+"</li>");
@@ -29,23 +34,6 @@ $(document).ready(function(){
 
 
 //Back-End
-function clearForm(){
-  document.getElementById("pingPongForm").reset();
-}
-
-function inputFocus(){
-  document.getElementById("userInput").focus();
-}
-
-function errorHandler(num){
-  if (!Number.isInteger(num) || num < 0){
-      clearForm();
-      alert("I like numbers. Please insert a valid number :)");
-      inputFocus();
-      return false;
-    }
-}
-
 function checkThree(num){
   if(num%3===0){
 		return true;
@@ -62,4 +50,12 @@ function checkBoth(num){
   if(checkThree(num) && checkFive(num)){
 		return true;
     }
+}
+
+function clearForm(){
+  document.getElementById("pingPongForm").reset();
+}
+
+function inputFocus(){
+  document.getElementById("userInput").focus();
 }
